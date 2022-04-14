bd3c9b393fdf9ec63dc18825048c7639f7a458ff
NeverMind109 <vladgladkih123@gmail.com>
2022-04-14 21:54:46 +0300

    Created: Added Examples Section

--- app/js/main.js
+++ app/js/main.js
@@ -31,4 +31,32 @@ $(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
   });
+
+  const mainCarousel = new Carousel(document.querySelector("#mainCarousel"), {
+    Dots: false,
+  });
+  // Thumbnails
+  const thumbCarousel = new Carousel(document.querySelector("#thumbCarousel"), {
+    Sync: {
+      target: mainCarousel,
+      friction: 0,
+    },
+    Dots: false,
+    Navigation: false,
+    center: true,
+    slidesPerPage: 1,
+    infinite: true,
+  });
+  // Customize Fancybox
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    Carousel: {
+      on: {
+        change: (that) => {
+          mainCarousel.slideTo(mainCarousel.findPageForSlide(that.page), {
+            friction: 0,
+          });
+        },
+      },
+    },
+  });
 });
